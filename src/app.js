b477e251522cb67f4c999ff96d5e1298e78faff5
miguelurrieta166 <miguelurrieta166@gmail.com>
2026-02-02 15:43:58 +0000

    cambios por M.U

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let rdmWho = Math.floor(Math.random() * who.length);
+  let rdmAction = Math.floor(Math.random() * action.length);
+  let rdmWhat = Math.floor(Math.random() * what.length);
+  let rdmWhen = Math.floor(Math.random() * when.length)
+
+  let finalExcuse= who[rdmWho] + "The dog" + action[rdmAction] + "ate"+ what [rdmWhat] +" my homework" + when[rdmWhen]; 
+
+ 
+  document.querySelector("#excuse").innerHTML= finalExcuse;
+ };
